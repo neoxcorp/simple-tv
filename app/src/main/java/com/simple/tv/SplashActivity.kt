0be32a033cca12d7b4f998b43f12c9d46e428f11
Andrew Ilchenko <a4etko@gmail.com>
2018-04-27 14:25:10 +0300

    change logic for playStreamVideo

--- app/src/main/java/com/simple/tv/SplashActivity.kt
+++ app/src/main/java/com/simple/tv/SplashActivity.kt
@@ -1,7 +1,17 @@
 package com.simple.tv
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import kotlinx.android.synthetic.main.activity_splash.*
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 class SplashActivity : AppCompatActivity() {
 
@@ -9,8 +19,30 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
+
+        playStreamVideo("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
     }
 
     fun playStreamVideo(url: String) {
+        val dataSourceFactory
+                = DefaultDataSourceFactory(this,
+                                           Util.getUserAgent(this,
+                                                             "mediaPlayerSample"))
+
+        val videoTrackSelectionFactory
+                = AdaptiveTrackSelection.Factory(BandwidthMeter { 5000 })
+
+        val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+        val player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+
+        playerView.player = player
+        player.playWhenReady = true
+
+        val extractorsFactory = DefaultExtractorsFactory()
+
+        val mediaSource =
+                ExtractorMediaSource(Uri.parse(url),
+                                     dataSourceFactory, extractorsFactory, null, null)
+        player.prepare(mediaSource)
     }
 }
