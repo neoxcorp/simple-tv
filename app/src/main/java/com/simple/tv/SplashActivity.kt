90f7c3f51a100cb38549aa3bcb6224b708f14652
Andrew Ilchenko <a4etko@gmail.com>
2018-04-27 19:48:45 +0300

    add getContent

--- app/src/main/java/com/simple/tv/SplashActivity.kt
+++ app/src/main/java/com/simple/tv/SplashActivity.kt
@@ -3,8 +3,9 @@ package com.simple.tv
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
@@ -12,23 +13,42 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter
 import kotlinx.android.synthetic.main.activity_splash.*
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.koushikdutta.ion.Ion
 
 class SplashActivity : AppCompatActivity() {
+
+    private val TAG = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
 
-        // val videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-        val videoUrl = "http://playlist.onlybest.tv/ru/channel/d0f21a70397d3659fe259aa5e2a62107/70/index.m3u8"
-        playStreamVideo(videoUrl)
+        getContent()
+    }
+
+    fun getContent() {
+        Log.e(TAG, "getContent")
+
+        Ion.with(this)
+                .load(BuildConfig.API_URL)
+                .asJsonObject()
+                .setCallback({ e, result ->
+                    Log.i(TAG, "result: $result")
+                    Log.e(TAG, "exception: $e")
+
+                    if (result != null && !result.isJsonNull) {
+                        playStreamVideo(result["tv_list"].asJsonArray[0]
+                                                .asJsonObject["stream_url"].asString)
+                    }
+                })
     }
 
     fun playStreamVideo(url: String) {
+        Log.e(TAG, "getContent -> url: $url")
+
         val videoTrackSelectionFactory
                 = AdaptiveTrackSelection.Factory(BandwidthMeter { 5000 })
 
@@ -48,9 +68,9 @@ class SplashActivity : AppCompatActivity() {
 
         val hlsMediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(url))
 
-        val videoSource = ExtractorMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(Uri.parse(url))
-
         player.prepare(hlsMediaSource)
+
+        playerView.visibility = VISIBLE
+        progressBar.visibility = GONE
     }
 }
