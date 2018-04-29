7522a445dfac95109451c99e36f1b4ff40ce2519
Andrew Ilchenko <a4etko@gmail.com>
2018-04-29 21:14:53 +0300

    add PlayerActivity

---
+++ app/src/main/java/com/simple/tv/ui/PlayerActivity.kt
@@ -0,0 +1,13 @@
+package com.simple.tv.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+
+    private val TAG = "PlayerActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
