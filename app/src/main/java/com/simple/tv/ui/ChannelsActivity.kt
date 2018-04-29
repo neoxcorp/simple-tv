19ec8d8e47fe7b46e1e4de0b1e94445db305b360
Andrew Ilchenko <a4etko@gmail.com>
2018-04-29 21:13:39 +0300

    add ChannelsActivity

---
+++ app/src/main/java/com/simple/tv/ui/ChannelsActivity.kt
@@ -0,0 +1,13 @@
+package com.simple.tv.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChannelsActivity : AppCompatActivity() {
+
+    private val TAG = "ChannelsActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
