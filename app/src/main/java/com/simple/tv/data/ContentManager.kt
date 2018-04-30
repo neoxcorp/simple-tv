08f01d659b7d332da6e080840d940a65683533e0
Andrew Ilchenko <a4etko@gmail.com>
2018-04-30 12:32:44 +0300

    add parseResponse

--- app/src/main/java/com/simple/tv/data/ContentManager.kt
+++ app/src/main/java/com/simple/tv/data/ContentManager.kt
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import com.koushikdutta.ion.Ion
 import com.simple.tv.BuildConfig
+import com.simple.tv.data.dto.Channel
 import com.simple.tv.data.dto.Response
 import com.simple.tv.ui.channels.adapter.ChannelItem
 import com.simple.tv.ui.channels.types.Data
@@ -26,7 +27,7 @@ class ContentManager(val context: Context) {
     ) {
         Log.e(TAG, "getContent")
 
-        Ion.with(context)
+        /*Ion.with(context)
             .load(BuildConfig.API_URL)
             .`as`(object : TypeToken<Response>() {})
             .setCallback({ e, result ->
@@ -50,13 +51,55 @@ class ContentManager(val context: Context) {
                     Crashlytics.log(it.message)
                     error.invoke(it)
                 }
+            })*/
+
+        Ion.with(context)
+            .load(BuildConfig.API_URL)
+            .asJsonObject()
+            .setCallback({ e, result ->
+                Log.i(TAG, "result: $result")
+                context.toast("result: $result")
+
+                result?.let {
+                    success.invoke(convertToData(parseResponse(result)))
+                } ?: run {
+                    context.toast("Empty list")
+                    Crashlytics.logException(Exception("Empty list"))
+                    Crashlytics.log("Empty list")
+                    error.invoke(Exception("Empty list"))
+                }
+
+                Log.e(TAG, "exception: $e")
+                context.toast("exception: $e")
+
+                e?.let {
+                    Crashlytics.logException(it)
+                    Crashlytics.log(it.message)
+                    error.invoke(it)
+                }
             })
     }
 
     private fun parseResponse(json: JsonObject): Response {
         Log.e(TAG, "parseResponse -> json: $json")
+        context.toast("parseResponse -> json: $json")
 
-        return Response()
+        val channels = arrayListOf<Channel>()
+
+        val  tv_list = json["tv_list"].asJsonArray
+
+        tv_list?.let {
+            it.forEach {
+                val o = it.asJsonObject
+                channels.add(
+                    Channel(o["name"].asString,
+                        o["stream_url"].asString,
+                        o["image_url"].asString))
+            }
+
+        }
+
+        return Response(channels)
     }
 
     private fun convertToData(response: Response): Data {
