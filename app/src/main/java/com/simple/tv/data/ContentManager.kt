24b021b8a99ed7fc62b499e1e0aab7b05e0c7b27
Andrew Ilchenko <a4etko@gmail.com>
2018-04-30 16:45:26 +0300

    change getListChannels

--- app/src/main/java/com/simple/tv/data/ContentManager.kt
+++ app/src/main/java/com/simple/tv/data/ContentManager.kt
@@ -3,23 +3,14 @@ package com.simple.tv.data
 import android.content.Context
 import android.util.Log
 import com.crashlytics.android.Crashlytics
-import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import com.koushikdutta.ion.Ion
 import com.simple.tv.BuildConfig
-import com.simple.tv.R
 import com.simple.tv.data.dto.Channel
 import com.simple.tv.data.dto.Response
 import com.simple.tv.ui.channels.adapter.ChannelItem
 import com.simple.tv.ui.channels.types.Data
-import org.jetbrains.anko.toast
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 
 class ContentManager(val context: Context) {
 
@@ -35,106 +26,32 @@ class ContentManager(val context: Context) {
     ) {
         Log.e(TAG, "getContent")
 
-        /*Ion.with(context)
-            .load(BuildConfig.API_URL)
-            .`as`(object : TypeToken<Response>() {})
-            .setCallback({ e, result ->
-                Log.i(TAG, "result: $result")
-                context.toast("result: $result")
-
-                result?.let {
-                    success.invoke(convertToData(it))
-                } ?: run {
-                    context.toast("Empty list")
-                    Crashlytics.logException(Exception("Empty list"))
-                    Crashlytics.log("Empty list")
-                    error.invoke(Exception("Empty list"))
-                }
-
-                Log.e(TAG, "exception: $e")
-                context.toast("exception: $e")
-
-                e?.let {
-                    Crashlytics.logException(it)
-                    Crashlytics.log(it.message)
-                    error.invoke(it)
-                }
-            })*/
-
-        /*Ion.with(context)
+        Ion.with(context)
             .load(BuildConfig.API_URL)
             .asJsonObject()
             .setCallback({ e, result ->
                 Log.i(TAG, "result: $result")
-                context.toast("result: $result")
 
                 result?.let {
                     success.invoke(convertToData(parseResponse(result)))
                 } ?: run {
-                    context.toast("Empty list")
-                    Crashlytics.logException(Exception("Empty list"))
-                    Crashlytics.log("Empty list")
                     error.invoke(Exception("Empty list"))
                 }
 
                 Log.e(TAG, "exception: $e")
-                context.toast("exception: $e")
 
                 e?.let {
-                    Crashlytics.logException(it)
-                    Crashlytics.log(it.message)
                     error.invoke(it)
                 }
-            })*/
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BuildConfig.API_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-            .build()
-
-
-        val api = retrofit.create(Api::class.java)
-
-        // val call = api.getChannelsTwo(context.getString(R.string.source_id))
-        val call = api.getChannelsTwo(context.getString(R.string.source_id_two))
-
-        call.enqueue(object : Callback<Response> {
-            override fun onFailure(call: Call<Response>?, e: Throwable?) {
-                Log.e(TAG, "exception: $e")
-                context.toast("exception: $e")
-
-                e?.let {
-                    Crashlytics.logException(it)
-                    Crashlytics.log(it.message)
-                    error.invoke(it)
-                }
-            }
-
-            override fun onResponse(
-                call: Call<Response>?,
-                response: retrofit2.Response<Response>?
-            ) {
-                Log.i(TAG, "call: $call")
-                Log.i(TAG, "response: $response")
-                context.toast("response: $response")
-
-                val resp = response?.body()
-
-                resp?.let {
-                    success.invoke(convertToData(resp))
-                }
-            }
-
-        })
+            })
     }
 
     private fun parseResponse(json: JsonObject): Response {
         Log.e(TAG, "parseResponse -> json: $json")
-        context.toast("parseResponse -> json: $json")
 
         val channels = arrayListOf<Channel>()
 
-        val  tv_list = json["tv_list"].asJsonArray
+        val tv_list = json["tv_list"].asJsonArray
 
         tv_list?.let {
             it.forEach {
